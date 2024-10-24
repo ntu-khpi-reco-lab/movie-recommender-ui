60e153e313f7077df29f19f3851a8b570aad5750
Liam <zhegalo200405@gmail.com>
2024-10-24 12:42:33 +0300

    like_funktion

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,10 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import likedMoviesReducer from './slices/likedMoviesSlice'; // Импортируем новый редюсер
+import likedMoviesReducer from './slices/likedMoviesSlice';
 import userReducer from './slices/userSlice';
 
 export const store = configureStore({
 	reducer: {
 		user: userReducer,
-		likedMovies: likedMoviesReducer, // Добавляем его в reducer
+		likedMovies: likedMoviesReducer,
 	},
 });
